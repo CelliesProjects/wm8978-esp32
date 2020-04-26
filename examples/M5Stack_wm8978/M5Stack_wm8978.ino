e17f9138b587fca4e132b65ac6e36d62bfd8b65d
Cellie <CelliesProjects@users.noreply.github.com>
2020-04-26 12:54:00 +0200

    Added an example.

---
+++ examples/M5Stack_wm8978/M5Stack_wm8978.ino
@@ -0,0 +1,49 @@
+#include <Audio.h>
+#include <wm8978.h> /* https://github.com/CelliesProjects/wm8978-esp32 */
+
+/* M5Stack Node WM8978 I2C pins */
+#define I2C_SDA     21
+#define I2C_SCL     22
+
+/* M5Stack Node I2S pins */
+#define I2S_BCK      5
+#define I2S_WS      13
+#define I2S_DOUT     2
+#define I2S_DIN     34
+
+/* M5Stack WM8978 MCLK gpio number and frequency */
+#define I2S_MCLKPIN  0
+#define I2S_MFREQ  (24 * 1000 * 1000)
+
+WM8978 dac;
+Audio audio;
+
+void setup() {
+  /* Setup wm8978 I2C interface */
+  if (!dac.begin(I2C_SDA, I2C_SCL)) {                              
+    ESP_LOGE(TAG, "Error setting up dac. System halted");
+    while (1) delay(100);
+  }
+  /* Setup wm8978 MCLK on gpio - for example M5Stack Node needs this clock on gpio 0 */
+  dac.setPinClockFreq(I2S_MCLKPIN, I2S_MFREQ);                     
+
+  /* Setup wm8978 I2S interface */
+  audio.setPinout(I2S_BCK, I2S_WS, I2S_DOUT, I2S_DIN);             
+
+  WiFi.begin("xxx", "xxx");
+  while (!WiFi.isConnected()) {
+    delay(10);
+  }
+  ESP_LOGI(TAG, "Connected");
+
+  ESP_LOGI(TAG, "Starting MP3...\n");
+  audio.connecttohost("http://icecast.omroep.nl/3fm-bb-mp3");
+
+  dac.setSPKvol(40); /* max 63 */
+  dac.setHPvol(32, 32);
+}
+
+void loop() {
+  audio.loop();
+  //delay(1);
+}
