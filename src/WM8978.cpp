e86ab8cc459fd04f2e6ef711cbcaed7415f0f3c8
Hendrik Langer <hendrik+dev@xd0.de>
2021-10-12 17:58:33 +0200

    enable high pass filter on default

--- src/WM8978.cpp
+++ src/WM8978.cpp
@@ -56,7 +56,7 @@ uint8_t WM8978::Init(void)
   Write_Reg(48, 1 << 8);  //R48设置,PGABOOSTR,右通道MIC获得20倍增益
   Write_Reg(49, 1 << 1);  //R49,TSDEN,开启过热保护
   Write_Reg(10, 1 << 3);  //R10,SOFTMUTE关闭,128x采样,最佳SNR
-  Write_Reg(14, 1 << 3);  //R14,ADC 128x采样率
+  Write_Reg(14, 1 << 3 | 1 << 8);  //R14,ADC 128x采样率 and enable high pass filter (3.7Hz cut-off)
   return 0;
 }
 
