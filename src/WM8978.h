71b56bf170e5e753acd3ed082264953957ba583d
Cellie <CelliesProjects@users.noreply.github.com>
2026-03-04 23:30:22 +0100

    formatting

--- src/WM8978.h
+++ src/WM8978.h
@@ -3,39 +3,39 @@
 
 #include <stdio.h>
 
-#define WM8978_ADDR   0X1A  //WM8978��������ַ,�̶�Ϊ0X1A
+#define WM8978_ADDR 0X1A // WM8978 i2c address
 
-#define EQ1_80Hz      0X00
-#define EQ1_105Hz     0X01
-#define EQ1_135Hz     0X02
-#define EQ1_175Hz     0X03
+#define EQ1_80Hz 0X00
+#define EQ1_105Hz 0X01
+#define EQ1_135Hz 0X02
+#define EQ1_175Hz 0X03
 
-#define EQ2_230Hz     0X00
-#define EQ2_300Hz     0X01
-#define EQ2_385Hz     0X02
-#define EQ2_500Hz     0X03
+#define EQ2_230Hz 0X00
+#define EQ2_300Hz 0X01
+#define EQ2_385Hz 0X02
+#define EQ2_500Hz 0X03
 
-#define EQ3_650Hz     0X00
-#define EQ3_850Hz     0X01
-#define EQ3_1100Hz    0X02
-#define EQ3_14000Hz   0X03
+#define EQ3_650Hz 0X00
+#define EQ3_850Hz 0X01
+#define EQ3_1100Hz 0X02
+#define EQ3_14000Hz 0X03
 
-#define EQ4_1800Hz    0X00
-#define EQ4_2400Hz    0X01
-#define EQ4_3200Hz    0X02
-#define EQ4_4100Hz    0X03
+#define EQ4_1800Hz 0X00
+#define EQ4_2400Hz 0X01
+#define EQ4_3200Hz 0X02
+#define EQ4_4100Hz 0X03
 
-#define EQ5_5300Hz    0X00
-#define EQ5_6900Hz    0X01
-#define EQ5_9000Hz    0X02
-#define EQ5_11700Hz   0X03
+#define EQ5_5300Hz 0X00
+#define EQ5_6900Hz 0X01
+#define EQ5_9000Hz 0X02
+#define EQ5_11700Hz 0X03
 
 class WM8978
 {
-  public:
+public:
     WM8978() {}
     ~WM8978() {}
-    bool begin(); /* use this function if you want to setup i2c before */
+    bool begin(); /* use this function if you want or need to setup i2c before starting the dac*/
     bool begin(const uint8_t sda, const uint8_t scl, const uint32_t frequency = 100000);
     void cfgADDA(uint8_t dacen, uint8_t adcen);
     void cfgInput(uint8_t micen, uint8_t lineinen, uint8_t auxen);
@@ -57,7 +57,7 @@ class WM8978
     void setALC(uint8_t enable, uint8_t maxgain, uint8_t mingain);
     void setHPF(uint8_t enable);
 
-  private:
+private:
     uint8_t Init(void);
     uint8_t Write_Reg(uint8_t reg, uint16_t val);
     uint16_t Read_Reg(uint8_t reg);
